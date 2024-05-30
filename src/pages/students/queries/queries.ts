e5e91710c0dc362a78956b2ccf01e0f8f447376a
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-05-31 00:07:45 +0700

    feat: remove students data

--- src/pages/students/queries/queries.ts
+++
@@ -1,9 +0,0 @@
-import { getStudents } from '@/lib/api';
-import { useQuery } from '@tanstack/react-query';
-
-export const useGetStudents = (offset, pageLimit, country) => {
-  return useQuery({
-    queryKey: ['students', offset, pageLimit, country],
-    queryFn: async () => getStudents(offset, pageLimit, country)
-  });
-};
